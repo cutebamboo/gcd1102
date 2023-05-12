692625ea7440bbb3031f237474e29e19a2f88b8f
Tran Thanh Truc <tructt16@fe.edu.vn>
2023-05-12 17:25:03 +0700

    Day1 - Web Modified to HTML

--- webapp.js
+++ webapp.js
@@ -4,8 +4,8 @@ const hostname = "localhost";
 
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
-    res.setHeader("Content-Type","application/json")
-    res.end('{"message" : "hello world"}');
+    res.setHeader("Content-Type","text/html")
+    res.end("<h1>Hello World</h1><p>This is a HTML response</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
 });
 
 server.listen(port,hostname,()=>{
