302600fc0213e4bf96702972168226793370cc6f
Tran Thanh Truc <tructt16@fe.edu.vn>
2023-05-17 15:29:55 +0700

    This version includes Home and About page

--- webapp.js
+++ webapp.js
@@ -5,7 +5,21 @@ const hostname = "localhost";
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader("Content-Type","text/html")
-    res.end("<h1>Hello World</h1><p>This is a HTML response</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
+    
+    switch (req.url){
+        case "/home":
+            res.writeHead(200);
+            res.end("<h1> This is Home page</h1>");
+            break;
+        case "/about":
+            res.writeHead(200);
+            res.end("<h1> This is About page</h1>");
+            break;
+        default:
+            res.end("<h1>Hello World</h1><p>This is a HTML response</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
+            break;
+    }
+
 });
 
 server.listen(port,hostname,()=>{
