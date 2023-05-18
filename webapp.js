63a70250fb7cecd6b58134651a0b030c0a6772aa
Tran Thanh Truc <tructt16@fe.edu.vn>
2023-05-18 22:05:11 +0700

    Add function read a file to webapp. readFileSync is used

--- webapp.js
+++ webapp.js
@@ -2,6 +2,9 @@ const http = require("http");
 const dt = require('./mymodule');
 const port = 3000;
 const hostname = "localhost";
+const fobj= require('fs');
+
+
 
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
@@ -20,6 +23,10 @@ const server = http.createServer((req,res)=>{
             res.writeHead(200);
             res.end(`<h1> This is About page ${dt.congtrunhanchia(10,200,"+")} <h1>`)
             break;
+        case "/literature":
+            res.writeHead(200,"Content-Type","text/plain");
+            const fcontent = fobj.readFileSync('./alice.txt');
+            res.end(`${fcontent.toString()}`);
         default:
             res.end("<h1>Hello World</h1><p>This is a HTML response</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
             break;
