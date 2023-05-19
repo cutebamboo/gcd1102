7faa237881e2c7b9d54d12cb014c92427010f404
Tran Thanh Truc <tructt16@fe.edu.vn>
2023-05-19 13:05:18 +0700

    A Test version with fopen

--- webapp.js
+++ webapp.js
@@ -27,11 +27,41 @@ const server = http.createServer((req,res)=>{
             res.writeHead(200,"Content-Type","text/plain");
             const fcontent = fobj.readFileSync('./alice.txt');
             res.end(`${fcontent.toString()}`);
+            break;
+        case "/createfile":
+            
+            // check if this file already exists or not
+            var buff = new Buffer.alloc(2048);
+            fobj.open('./alice.txt','r',(err,fd)=>{
+                if (err){
+                   return console.log(`${err.message}`);
+                }
+                fobj.read(fd,buff,0,buff.length,0,()=>{
+                    res.end(`<p> ${buff.toString('ascii')} </p>`);
+                    return;});
+                //console.log(buff.toString('ascii'));
+                res.write("<h1>Bonjour !!!</h1>");
+                res.write("<h1>File already existed</h1><p>This is a HTML response</p> <ol>");
+                
+            });
+            res.writeHead(200,"Content-Type","text/html");
+            res.write("<h1>Hello World</h1>");
+            
+            
+
+            //res.write(`<p> ${buff.toString('ascii')} </p>`,()=>{return;});
+            //console.log(buff.toString('ascii'));
+
+            /*
+                //console.log(buff.toString('ascii'));
+                //res.write("<h1>File already existed</h1><p>This is a HTML response</p> <ol>",()=>{return;});
+            */
+                break;
         default:
             res.end("<h1>Hello World</h1><p>This is a HTML response</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
             break;
     }
-
+return;
 });
 
 server.listen(port,hostname,()=>{
